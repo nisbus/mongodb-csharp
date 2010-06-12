02a07bf936a89b3647136c73ceb8792a2d17fdf6
Steve Wagner <lanwin@lanwin.de>
2010-06-12 22:10:46 +0800

    Made linq stuff internal.

--- source/MongoDB/Linq/MongoQueryProvider.cs
+++ source/MongoDB/Linq/MongoQueryProvider.cs
@@ -12,7 +12,7 @@ namespace MongoDB.Linq
     /// <summary>
     /// 
     /// </summary>
-    public class MongoQueryProvider : IQueryProvider
+    internal class MongoQueryProvider : IQueryProvider
     {
         private readonly string _collectionName;
         private readonly IMongoDatabase _database;
